cc9c13f11cdcb1b483fa79a0f76111f104266e09
Elias Breitenbach <elias.b@me.com>
2023-07-08 14:11:02 +0200

    Create a project on github

---
+++ Nevergram/NevergramApp.swift
@@ -0,0 +1,17 @@
+//
+//  NevergramApp.swift
+//  Nevergram
+//
+//  Created by Elias Breitenbach on 08.07.23.
+//
+
+import SwiftUI
+
+@main
+struct NevergramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
